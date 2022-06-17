f025903aabaccddfa36b2fd968ee84b0371020f7
connorpeck <90285369+connorpeck@users.noreply.github.com>
2022-06-17 18:04:10 -0500

    hook for feeling

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react';
 
 function Feeling( props ){
-    const [ hook, setHook ] = useState( null )
+    const [ feeling, setFeeling ] = useState( null );
+
+    const collectFeeling = () => {
+        console.log(event.target.value);
+        setFeeling('Feelings: ' + event.target.value);
+    }
+    const storeFeeling = () => {
+        console.log(feeling);
+    }
     return (
         <div>
             <h2>How are you feeling today?</h2>
-            <input type="text" />
-            <button>Next</button>
+            <input onChange={collectFeeling}type="text" />
+            <button onClick={storeFeeling}>Next</button>
             {/* <p>Props: { JSON.stringify( props ) }</p> */}
         </div>
     );
